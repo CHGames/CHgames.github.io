7cced8aba3bf8124ffaa58905cb90e51a7e960d9
CHGames <greenj2@wis.edu.hk>
2015-11-13 11:02:25 +0800

    game

---
+++ game/js/index.js
@@ -0,0 +1,27 @@
+function game() {
+alert("Welcome young ninja to NINJA MADNESS")
+alert("Many young ones start their journey")
+alert("Not many finish it")
+alert("Let us get started")
+
+var q1 = prompt("You see two evil minions, what do you chose to do? Sneak or Fight?");
+
+if (q1==="Fight") {
+alert("Two overpower one, you die, game over.");  
+  
+}
+else {
+alert("Well done, you have survived... for now."); 
+    var q2 = prompt("You see another evil minion tying his shoelaces, do you Kill him or Run past him?");
+  if (q2==="Run past") {
+    alert("You died, tough, deal with it kid:P");
+    
+  }
+  else {
+    alert("You sliced his head off, well done you are still alive");
+    var q3 = prompt("You see the evil apprentice, do you sneak up from behind or go in stupidly?");
+    if (p3==="")
+  }
+}
+
+}
